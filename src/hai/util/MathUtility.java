d99ee4f5c734882ab606a15dcc1b6aff70fa601d
lamtl1301 <lamtl1301@gmail.com>
2020-11-18 08:00:10 +0700

    Optimize the getF() by using recursion

--- src/hai/util/MathUtility.java
+++ src/hai/util/MathUtility.java
@@ -10,28 +10,42 @@ package hai.util;
  * @author Admin
  */
 public class MathUtility {
+
     //Math này fake JDK
     // JDK có .abs(), .sqrt(), .sin(), .Pi
     public static final double PI = 3.1415;
-    
+
     //hàm tính giai thừa n! = 1.2.3.4...n;
     //mình ko tính giai thừa âm
     //21! cực lớn, tràn kiểu long
     //int: tối đa 2 tỷ .....
     // hàm tính giai thừa trong giới hạn đầu vào từ 0...20
-    public static long getFactorial(int n){
-        if (n < 0 || n > 20){
-            throw new IllegalArgumentException("n must be between 0..20");
+//    public static long getFactorial(int n){
+//        if (n < 0 || n > 20){
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        // ko dùng else
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+    //viết lại dệ quy
+    // gọi lf chính mình với quy mô khác
+    // đẹ quy phải có điểm dừng, không thì bị lặp vô tận
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be betwwen 0..20");
         }
         if (n == 0 || n == 1) {
             return 1;
         }
-        // ko dùng else
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
     }
-
+    //minh họa khái niệm Refactoring - tối ưu, chỉnh sửa lại code cho tốt hơn
+    //minh họa khái niệm Regrression Test, test hồi quy , test lại những gì đã từng test XANH, check xem code còn ngày xưa không
 }
